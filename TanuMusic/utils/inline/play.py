80ed483b5947ffaf5da55ccbcbd9285c0405782c
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-27 12:10:42 +0530

    Update play.py

--- TanuMusic/utils/inline/play.py
+++ TanuMusic/utils/inline/play.py
@@ -97,6 +97,15 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
+InlineKeyboardButton(
+
+                text="๏ ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ ๏",
+
+                url=f"https://t.me/{self.username}?startgroup=true",
+
+            ),
+        ],
+        [
             InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="၊၊", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
