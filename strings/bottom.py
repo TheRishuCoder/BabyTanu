ec6ef2982e58c9c547ebfd0354c0e7324c0dd66b
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-27 12:49:09 +0530

    Update strings

---
+++ strings/bottom.py
@@ -0,0 +1,7 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram import Client, filters, enums 
+
+class BUTTONS(object):
+    ABUTTON = [[InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/ur_support07")],[InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/ur_rishu_143"),
+    InlineKeyboardButton("˹ ᴧʟʟ ʙᴏᴛ ˼", url="https://t.me/Vip_robotz/4")],[InlineKeyboardButton("↺ ʙᴧᴄᴋ ↻", callback_data=f"settingsback_helper"),
+    ]]
